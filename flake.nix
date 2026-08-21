75e6472dec856c2f2ef40f1dfcbf116637e255c9
BrockoliniMorgan <brockjamesmorgan@gmail.com>
2026-08-22 00:18:39 +1000

    Updated flake to use multiple systems

--- flake.nix
+++ flake.nix
@@ -1,6 +1,7 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
     ros2nix_repo = {
       url = "github:wentasah/ros2nix";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -12,28 +13,32 @@
       self,
       nixpkgs,
       ros2nix_repo,
+      flake-utils,
     }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs {
-        inherit system;
-        overlays = [
-          (final: prev: {
-            ros2nix = ros2nix_repo.packages.${system}.default;
-          })
-        ];
-      };
-    in
-    {
-      devShells.${system}.default = pkgs.mkShellNoCC {
-        name = "ros-NUR-helper";
-        inputsFrom = with self.packages.${system}; [
-          ros-NUR-helper
-        ];
-      };
-      packages.${system} = {
-        ros-NUR-helper = pkgs.callPackage ./package.nix { };
-        default = self.packages.${system}.ros-NUR-helper;
-      };
-    };
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        system = "x86_64-linux";
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            (final: prev: {
+              ros2nix = ros2nix_repo.packages.${system}.default;
+            })
+          ];
+        };
+      in
+      {
+        devShells.default = pkgs.mkShellNoCC {
+          name = "ros-NUR-helper";
+          inputsFrom = with self.packages.${system}; [
+            ros-NUR-helper
+          ];
+        };
+        packages = {
+          ros-NUR-helper = pkgs.callPackage ./package.nix { };
+          default = self.packages.${system}.ros-NUR-helper;
+        };
+      }
+    );
 }
